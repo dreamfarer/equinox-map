8d312a2bc1ae2cebeb86621e114cd812162febf6
dreamfarer <dreamfarer@pm.me>
2025-05-12 20:19:41 +0200

    chore: add website title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import "./global.css";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: `equinoxmap\.app`,
+  title: "Equinox: Homecoming Interactive Map",
+  description: "Explore quests, resources, races and more with this interactive map for Equinox: Homecoming ",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
