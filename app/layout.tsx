237afa3fbc3f049768bc8c828e0f9052864fc3f7
dreamfarer <dreamfarer@pm.me>
2025-05-13 23:16:30 +0200

    feat: add site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,26 @@ import { ReactNode } from 'react';
 export const metadata = {
   title: 'Equinox: Homecoming Interactive Map',
   description:
-    'Explore quests, resources, races and more with this interactive map for Equinox: Homecoming ',
+    'Explore quests, resources, races and more with this interactive map for Equinox: Homecoming.',
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      {
+        url: '/android-chrome-192x192.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        type: 'image/png',
+        sizes: '512x512',
+      },
+      { url: '/favicon.ico', type: 'image/x-icon' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
