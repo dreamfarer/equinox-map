95a5881104c949c22b0abe10ea488a853befc489
dreamfarer <dreamfarer@pm.me>
2025-05-17 11:41:45 +0200

    refactor(Category): support more than two entries

--- app/components/filter/category.tsx
+++ app/components/filter/category.tsx
@@ -19,6 +19,11 @@ export default function Category({
   onToggle,
   entries,
 }: Props) {
+  const chunkedEntries: Entry[][] = [];
+  for (let i = 0; i < entries.length; i += 2) {
+    chunkedEntries.push(entries.slice(i, i + 2));
+  }
+
   return (
     <div className={styles.category}>
       <button
@@ -27,18 +32,20 @@ export default function Category({
       >
         {title}
       </button>
-      <div className={styles.row}>
-        {entries.map((entry, index) => (
-          <div key={index} className={styles.column}>
-            <button
-              onClick={entry.onToggle}
-              className={`${styles.button} ${styles.entry} ${entry.isActive ? styles.active : styles.inactive}`}
-            >
-              {entry.label}
-            </button>
-          </div>
-        ))}
-      </div>
+      {chunkedEntries.map((group, rowIndex) => (
+        <div key={rowIndex} className={styles.row}>
+          {group.map((entry, columnIndex) => (
+            <div key={columnIndex} className={styles.column}>
+              <button
+                onClick={entry.onToggle}
+                className={`${styles.button} ${styles.entry} ${entry.isActive ? styles.active : styles.inactive}`}
+              >
+                {entry.label}
+              </button>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
