e37f1b57f76ea5a19f693444c69e2867a83b6eff
dreamfarer <dreamfarer@pm.me>
2025-05-26 18:35:50 +0200

    chore: add heading for screen readers

---
+++ app/components/home.tsx
@@ -0,0 +1,38 @@
+'use client';
+import React, { useState } from 'react';
+import styles from './home.module.css';
+import { TMenu } from '@/types/menu';
+import { MarkerLayerProvider } from '../context/marker-layer';
+import Navbar from './navbar';
+import Filter from './filter';
+import Bookmarks from './bookmarks';
+import MapWrapper from './map-wrapper';
+
+export default function Home() {
+  const [selectedMenu, setSelectedMenu] = useState<TMenu>('filter');
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const openMenu = (menu: TMenu) => {
+    setSelectedMenu(menu);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen((v) => !v);
+  };
+
+  return (
+    <MarkerLayerProvider>
+      <div className={styles.home}>
+        <Navbar
+          selectedMenu={selectedMenu}
+          isMenuOpen={isMenuOpen}
+          onSelectMenu={openMenu}
+          onToggleMenu={toggleMenu}
+        />
+        {isMenuOpen && selectedMenu == 'filter' && <Filter />}
+        {isMenuOpen && selectedMenu == 'bookmarks' && <Bookmarks />}
+        <MapWrapper />
+      </div>
+    </MarkerLayerProvider>
+  );
+}
