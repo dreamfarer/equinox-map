257bb62804b939cf7127b5d0fe408e6dbf4e3190
dreamfarer <dreamfarer@pm.me>
2026-01-30 15:04:58 +0100

    chore: make marker conversion simpler

---
+++ types/marker-source.ts
@@ -0,0 +1,10 @@
+export type MarkerSource = {
+    title?: string;
+    subtitle?: string;
+    id?: string;
+    foreignId?: string;
+    map: string;
+    icon?: string;
+    x: number;
+    y: number;
+};
