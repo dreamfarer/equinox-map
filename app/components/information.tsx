b4d9340d1b318be088d428a44c17fb58b6bb2d81
dreamfarer <dreamfarer@pm.me>
2026-02-24 09:01:30 +0100

    feat: add information menu page

---
+++ app/components/information.tsx
@@ -0,0 +1,64 @@
+import styles from '@/app/components/information.module.css';
+
+export default function Information() {
+    return (
+        <>
+            <h1>Need Help, found a bug or want to contribute?</h1>
+            <p>
+                Don&#39;t hesitate to{' '}
+                <a
+                    className={styles.link}
+                    href="https://github.com/dreamfarer/equinox-map/issues/new"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    open an issue
+                </a>{' '}
+                on GitHub and I will gladly try to assist you. Questions, bug
+                reports and contributions are always warmly welcome!
+            </p>
+            <button
+                className={styles.button}
+                onClick={() =>
+                    window.open(
+                        'https://github.com/dreamfarer/equinox-map/issues/new',
+                        '_blank',
+                        'noopener,noreferrer'
+                    )
+                }
+            >
+                Open Issue
+            </button>
+            <h1>Acknowledgements</h1>
+            <p>
+                Thanks to CookieFox, Laika, lil big guy, LupinchenBrause, 𝔉𝔢𝔢𝔩𝔦,
+                Onyx, Snowhawk, Sugertoxity, Yumemi, zaaap! and, of course, Blue
+                Scarab Entertainment, the studio behind Equinox: Homecoming —
+                without you wonderful people, this interactive map would not
+                have been possible. Visit{' '}
+                <a
+                    className={styles.link}
+                    href="https://github.com/dreamfarer/equinox-map"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub
+                </a>{' '}
+                for further details.
+            </p>
+            <button
+                className={styles.button}
+                onClick={() =>
+                    window.open(
+                        'https://github.com/dreamfarer/equinox-map/',
+                        '_blank',
+                        'noopener,noreferrer'
+                    )
+                }
+            >
+                Open Source Code
+            </button>
+            <button className={styles.button}>Restart Tutorial</button>
+        </>
+    );
+}
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,27 +1,44 @@
 'use client';
 
 import { useMenuState } from '@/app/context/menu-state-context';
-import { ListDashesIcon } from '@phosphor-icons/react';
+import { InfoIcon, ListDashesIcon } from '@phosphor-icons/react';
 import styles from '@/app/components/navbar.module.css';
 
 export default function Navbar() {
-    const { activeMenuName, setIsMenuOpen, setActiveMenuName } = useMenuState();
+    const { activeMenuName, isMenuOpen, setIsMenuOpen, setActiveMenuName } =
+        useMenuState();
 
     return (
         <div className={styles.navbar}>
             <button
                 onClick={() => {
                     setActiveMenuName('filter');
+                    if (activeMenuName !== 'filter' && isMenuOpen) return;
                     setIsMenuOpen((prev) => !prev);
                 }}
                 className={`${styles.button} ${
-                    activeMenuName === 'filter'
+                    activeMenuName === 'filter' && isMenuOpen
                         ? styles.inactive
                         : styles.active
                 }`}
                 aria-label="Show filter"
             >
                 <ListDashesIcon size="2em" />
+            </button>
+            <button
+                onClick={() => {
+                    setActiveMenuName('information');
+                    if (activeMenuName !== 'information' && isMenuOpen) return;
+                    setIsMenuOpen((prev) => !prev);
+                }}
+                className={`${styles.button} ${
+                    activeMenuName === 'information' && isMenuOpen
+                        ? styles.inactive
+                        : styles.active
+                }`}
+                aria-label="Show filter"
+            >
+                <InfoIcon size="2em" />
             </button>
         </div>
     );
