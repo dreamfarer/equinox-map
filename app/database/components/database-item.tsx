072c06bd7551d3d4a5f625aa45223b452167eb06
dreamfarer <dreamfarer@pm.me>
2026-03-08 13:08:40 +0100

    feat: implement database item component

---
+++ app/database/components/database-item.tsx
@@ -0,0 +1,87 @@
+import Image from 'next/image';
+import styles from '@/app/database/components/database-item.module.css';
+
+type Props = {
+    name: string;
+    category: string;
+    statsAmount: number;
+    statsType: string;
+    level: number;
+    faction: string;
+    cost: number;
+    currency: string;
+    shop: string;
+    upgradeAmount: number;
+    upgradeItem: string;
+    imagePath: string;
+};
+
+export default function DatabaseItem({
+    name,
+    category,
+    statsAmount,
+    statsType,
+    level,
+    faction,
+    cost,
+    currency,
+    shop,
+    upgradeAmount,
+    upgradeItem,
+    imagePath,
+}: Props) {
+    return (
+        <div className={styles.item}>
+            <div className={styles.header}>
+                <h1 className={styles.title}>{name}</h1>
+                <h2 className={`${styles.text} ${styles.light}`}>{category}</h2>
+            </div>
+            <div className={styles.image}>
+                <Image
+                    src={imagePath}
+                    alt={name}
+                    fill
+                    style={{ objectFit: 'contain' }}
+                />
+            </div>
+            <div className={styles.information}>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Stats:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        +{statsAmount} {statsType}
+                    </p>
+                </div>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Reputation:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        {level}
+                    </p>
+                </div>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Faction:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        {faction}
+                    </p>
+                </div>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Cost:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        {cost} {currency}
+                    </p>
+                </div>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Shop:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        {shop}
+                    </p>
+                </div>
+                <div className={styles.entry}>
+                    <h2 className={styles.text}>Upgrade:</h2>
+                    <p className={`${styles.text} ${styles.rightAlign}`}>
+                        {upgradeAmount} {upgradeItem}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
