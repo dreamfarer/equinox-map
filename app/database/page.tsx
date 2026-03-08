be2b5c0da6312b4f8063bc1023caac076c0165a0
dreamfarer <dreamfarer@pm.me>
2026-03-08 13:13:48 +0100

    feat: show exemplary database item

--- app/database/page.tsx
+++ app/database/page.tsx
@@ -1,3 +1,25 @@
-export default function RootPage() {
-    return <h1>Hello World</h1>;
+import styles from '@/app/database/page.module.css';
+import DatabaseItem from '@/app/database/components/database-item';
+
+export default function DatabasePage() {
+    return (
+        <div className={styles.page}>
+            <div className={styles.grid}>
+                <DatabaseItem
+                    name={'Western Saddle (White)'}
+                    category={'Saddle'}
+                    statsAmount={40}
+                    statsType={'Riding'}
+                    level={6}
+                    faction={'Alderwood Equestrians'}
+                    cost={300}
+                    currency={'Silver Sols'}
+                    shop={"Wendy's Tack Shop"}
+                    upgradeAmount={12}
+                    upgradeItem={'Apples'}
+                    imagePath={'/icon/64/kathy.webp'}
+                />
+            </div>
+        </div>
+    );
 }
