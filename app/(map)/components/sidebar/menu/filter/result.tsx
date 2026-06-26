e6bdbf94e3900836973f9deb44267afbc64a7726
dreamfarer <dreamfarer@pm.me>
2026-06-26 12:36:13 +0200

    style: shorten and consolidate CSS styles

--- app/(map)/components/sidebar/menu/filter/result.tsx
+++ app/(map)/components/sidebar/menu/filter/result.tsx
@@ -9,13 +9,11 @@ type Props = {
 
 export default function Result({ title, category, count, onSelect }: Props) {
     return (
-        <div className={styles.result} onClick={onSelect}>
-            <div className={styles.information}>
-                <div className={styles.title}>
-                    {title} {count && count > 1 && <span>({count})</span>}
-                </div>
-                <div className={styles.subtitle}>{category}</div>
-            </div>
-        </div>
+        <button className={styles.result} onClick={onSelect}>
+            <h2>
+                {title} {count && count > 1 && <span>({count})</span>}
+            </h2>
+            <h3>{category}</h3>
+        </button>
     );
 }
